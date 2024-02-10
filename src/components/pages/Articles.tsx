6a04960c8be06d1ef9998a9b4eb98ef089e77e09
Nouman Altaf <nouman@markinson.com.au>
2024-02-10 23:45:47 +0400

    condition added

--- src/components/pages/Articles.tsx
+++ src/components/pages/Articles.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ArticleGrid from '../ArticleGrid';
 import Typography from '@mui/material/Typography';
 import { useFetchMostViewedArticles } from '../../api/article';
+import { isNull } from '../../helpers';
 
 const Articles = () => {
     const articles = useFetchMostViewedArticles();
@@ -11,6 +12,7 @@ const Articles = () => {
             <Typography variant="h3" gutterBottom textAlign={'center'}>
                 Articles
             </Typography>
+            <p>{articles.isFetched && isNull(articles.data) && 'Check API_KEY, unable to fetch data'}</p>
             <ArticleGrid loading={articles.isLoading} articles={articles?.data?.results ?? []} />
         </div>
     );

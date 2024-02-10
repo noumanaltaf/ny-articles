a1930102fec7e51cb20a34975c8cc6d351598246
Nouman Altaf <nouman@markinson.com.au>
2024-02-10 23:13:05 +0400

    test files ignored

--- src/components/DetailContent/DetailContent.test.tsx
+++ src/components/DetailContent/DetailContent.test.tsx
@@ -1,10 +1,10 @@
+import React from 'react';
+
 import { render } from "@testing-library/react";
 import DetailContent from "./DetailContent";
 import { dummyArticle } from "../../dummyData";
 
 describe('renders article detail', () => {
-
-
 
     it('should have title', () => {
         const { getByText } = render(<DetailContent article={dummyArticle} />);
